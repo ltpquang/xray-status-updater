16d1de52864f8e54106ec03302f700d6191d257f
Quang Le <phuc.quang102@gmail.com>
2020-07-25 21:02:12 +0700

    Try catch networking exception

--- src/main/kotlin/com/ltpquang/xray/client/XrayClient.kt
+++ src/main/kotlin/com/ltpquang/xray/client/XrayClient.kt
@@ -10,7 +10,6 @@ import okhttp3.Request
 import okhttp3.internal.EMPTY_REQUEST
 import okhttp3.logging.HttpLoggingInterceptor
 
-
 /**
  * Created by Quang Le (quangltp) on 7/25/20
  *
@@ -38,27 +37,37 @@ class XrayClient(host: String, username: String, password: String) {
             .build()
     }
 
-    private fun getTestRun(testIssueKey: String, testExecIssueKey: String): TestRun {
-        val req = Request.Builder()
-            .get()
-            .url(xrayUrl.getTestRunUrl(testIssueKey, testExecIssueKey))
-            .build()
-        val res = client.newCall(req).execute()
-        val result = Gson().fromJson(res.body!!.charStream(), TestRun::class.java)
-        print(result)
-        return result
+    fun setStatus(testIssueKey: String, testExecIssueKey: String, status: Status) {
+        val testRun = getTestRun(testIssueKey, testExecIssueKey) ?: return
+        setTestRunStatus(testRun.id, status)
+    }
+
+    private fun getTestRun(testIssueKey: String, testExecIssueKey: String): TestRun? {
+        try {
+            val req = Request.Builder()
+                .get()
+                .url(xrayUrl.getTestRunUrl(testIssueKey, testExecIssueKey))
+                .build()
+            val res = client.newCall(req).execute()
+            if (res.code != 200) {
+                return null
+            }
+            return Gson().fromJson(res.body!!.charStream(), TestRun::class.java)
+        } catch (e: Exception) {
+            e.printStackTrace()
+            return null
+        }
     }
 
     private fun setTestRunStatus(id: Int, status: Status) {
-        val req = Request.Builder()
-            .put(EMPTY_REQUEST)
-            .url(xrayUrl.updateTestRunStatusUrl(id, status))
-            .build()
-        val res = client.newCall(req).execute()
-    }
-
-    fun setStatus(testIssueKey: String, testExecIssueKey: String, status: Status) {
-        val testRun = getTestRun(testIssueKey, testExecIssueKey)
-        setTestRunStatus(testRun.id, status)
+        try {
+            val req = Request.Builder()
+                .put(EMPTY_REQUEST)
+                .url(xrayUrl.updateTestRunStatusUrl(id, status))
+                .build()
+            client.newCall(req).execute()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
 }
